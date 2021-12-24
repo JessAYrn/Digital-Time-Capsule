58dc6e5e6b6447e7f93d7d9f5052e65483d3c8e3
Jesse Williams <jessewilliams@Jesses-MacBook-Pro.local>
2021-12-24 12:26:43 -0600

    routes hooked up. Still need to make homepage

--- src/dtc_assets/src/index.jsx
+++ src/dtc_assets/src/index.jsx
@@ -1,6 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import HomePage from './HomePage';
 import App from './App';
 import configureStore from './configureStore';
 
@@ -12,6 +18,11 @@ const store = configureStore();
 
 ReactDOM.render(
 <Provider store={store}>
-    <App />
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<HomePage />}/>
+            <Route path="/app" element={<App />}/>
+        </Routes>
+    </BrowserRouter>
 </Provider>
 , document.getElementById('root'));
