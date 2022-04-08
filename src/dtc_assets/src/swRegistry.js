e5d64d38521ee1378f0092966fa0ec0d1c23bc6a
Jesse Williams <jessewilliams@Jesses-MBP.lan>
2022-04-07 23:28:46 -0500

    change

--- src/dtc_assets/src/swRegistry.js
+++ src/dtc_assets/src/swRegistry.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/Digital-Time-Capsule/src/dtc_assets/src//sw.js')
+    navigator.serviceWorker.register('https://cqjyx-qqaaa-aaaap-qaakq-cai.ic0.app/src/dtc_assets/src/sw.js')
         .then((reg) => {
             console.log('Service Worker Registered: ', reg);
         })
