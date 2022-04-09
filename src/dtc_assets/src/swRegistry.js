1cd6b048bd0e4af268bde0092aa6aec40ad35935
Jesse Williams <jessewilliams@Jesses-MacBook-Pro.local>
2022-04-09 10:28:11 -0500

    manifest file added

--- src/dtc_assets/src/swRegistry.js
+++
@@ -1,9 +0,0 @@
-if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('https://cqjyx-qqaaa-aaaap-qaakq-cai.ic0.app/src/dtc_assets/src/sw.js')
-        .then((reg) => {
-            console.log('Service Worker Registered: ', reg);
-        })
-        .catch((err) => {
-            console.log('Service Worker Not Registered: ', err);
-        });
-}
