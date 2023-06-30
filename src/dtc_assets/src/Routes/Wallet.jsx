5fa147fc21e2e979ccfc5980f5da741f10d94737
Jesse Williams <jessewilliams@Jesses-MacBook-Pro-2.local>
2023-06-30 12:46:51 -0500

    bug fix on loginPage with actorDispatch and actorState

--- src/dtc_assets/src/Routes/Wallet.jsx
+++ src/dtc_assets/src/Routes/Wallet.jsx
@@ -119,8 +119,8 @@ const WalletApp = () => {
                     accountState,
                     homePageDispatch,
                     homePageState,
-                    accountDispatch,
-                    accountState
+                    actorDispatch,
+                    actorState
                 }}
             >
                 {
