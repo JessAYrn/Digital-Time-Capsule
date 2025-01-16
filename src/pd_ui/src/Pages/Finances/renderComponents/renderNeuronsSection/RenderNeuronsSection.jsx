e3b988446ace7731d6bcdad83fb57d5bdd3353cb
Jesse Williams <jessewilliams@MacBookPro.attlocal.net>
2025-01-16 11:59:52 -0600

    RenderEmptyNeurons component created for when no neurons are staked within the DAO

--- src/pd_ui/src/Pages/Finances/renderComponents/renderNeuronsSection/RenderNeuronsSection.jsx
+++ src/pd_ui/src/Pages/Finances/renderComponents/renderNeuronsSection/RenderNeuronsSection.jsx
@@ -7,11 +7,16 @@ import Typography from "@mui/material/Typography";
 import RenderPreviewNeuron from "./RenderPreviewNeuron";
 import { AppContext } from "../../../../Context";
 import RenderInactiveNeuronsList from "./RenderInactiveNeuronsList";
+import CreateProposalForm from "../../../../proposals/CreateProposalForm";
+import { PROPOSAL_ACTIONS } from "../../../../proposals/utils";
+import ButtonField from "../../../../components/Button";
+import { BACKGROUND_COLOR } from "../../../../Theme";
+
 const RenderNeuronsSection = (props) => {
 
     const { treasuryState, setModalIsOpen, setModalProps } = useContext(AppContext);
         
-    const {activeNeurons, inactiveNeurons} = useMemo(() => {
+    const { activeNeurons, inactiveNeurons} = useMemo(() => {
         const activeNeurons = [];
         const inactiveNeurons = [];
         for (const [neuronId, neuronData] of treasuryState?.neurons?.icp) {
@@ -29,11 +34,33 @@ const RenderNeuronsSection = (props) => {
         });
     }
 
+    const onCreateNeuron = () => {
+        setModalIsOpen(true);
+        setModalProps({ 
+            headerComponent: <Typography variant="h6">Create Proposal</Typography>,
+            fullScreen: true,
+            components: [ 
+                <CreateProposalForm proposalAction={PROPOSAL_ACTIONS.CreateNeuron} proposalPayload={{}}/>
+            ] 
+        });
+    };
+
+    const RenderEmptyNeurons = () => {
+        return (
+            <Grid xs={12} display={"flex"} justifyContent={"center"} alignItems={"center"} width={"100%"} padding={0} flexDirection={"column"} paddingTop={"20px"}>
+                <Typography textAlign={"center"} >
+                    {"There are currently no neurons staked in this treasury. Neurons are used to stake ICP which allows this DAO to earn rewards and participate in the governance of the ICP blockchain."}
+                    </Typography>
+                <ButtonField gridSx={{backgroundColor: CONTRAST_COLOR, marginTop: "20px"}} color={BACKGROUND_COLOR} text={"Create Neuron"} onClick={onCreateNeuron}/>
+            </Grid>
+        )
+    }
+
     return (
         <Grid xs={12} width={"100%"} display={"flex"} justifyContent={"center"} alignItems={"center"} padding={0} flexDirection={"column"}>
             <CarouselComponent 
                 title={"Neurons"} 
-                defaultComponent={<Typography textAlign={"center"} component={"There are currently no neurons staked in this treasury"} />}
+                defaultComponent={<RenderEmptyNeurons/>}
             >
                 { activeNeurons.map(([neuronId, neuronData]) => { return (<RenderPreviewNeuron neuronData={neuronData} neuronId={neuronId}/>); }) }
             </CarouselComponent>
